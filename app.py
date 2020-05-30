41cb66d07f542170601aa8426dfacb9c02e15bb1
Piotr Talaga <piotrek.talaga@gmail.com>
2020-05-30 16:34:51 +0200

    API connection in progress

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
 from flask import Flask, render_template, request
+from helpers import getData
+
 app = Flask(__name__)
 
+url = 'api.openweathermap.org/data/2.5/weather?q={city name}&appid={your api key}'
 
 @app.route('/')
 def index():
-    mssg = "A pewnie, że tak"
-    return render_template("index.html", mssg = mssg)
+    return render_template("index.html")
+
+@app.route('/main')
+def main():
+    city = 'Las Vegas'
+    weatherData = getData(city)
+    print("weatherData: ")
+    print(weatherData)
+    return render_template("main.html")
+
+
 
 if __name__ == '__main__':
     app.run()
---
+++ helpers.py
@@ -0,0 +1,21 @@
+import requests
+
+
+def getData(value):
+    """Look up quote for symbol."""
+
+    # Contact API
+    try:
+        api_key = '4942d893e111cf68b4f2e9acbf544dfc'
+        response = requests.get(f"http://api.openweathermap.org/data/2.5/weather?q={value}&appid={api_key}")
+    except requests.RequestException as err:
+        print("jest exception")
+        print(err)
+        return None
+
+    # Parse response
+    try:
+        quote = response.json()
+        return quote
+    except (KeyError, TypeError, ValueError):
+        return None
